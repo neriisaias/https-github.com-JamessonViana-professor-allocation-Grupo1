2d8fa4a8cac698d9e51bcaffe04754354c380835
NairtonMenezes <nairtonmenezesjunior@gmail.com>
2024-02-24 16:06:22 -0300

    futuro erro.

--- src/main/java/com/projet/professor/allocation/grupoJava/ProfessorAllocationGrupoJavaApplication.java
+++ src/main/java/com/projet/professor/allocation/grupoJava/ProfessorAllocationGrupoJavaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationGrupoJavaApplication {
-
+// futuro conflito, dev 1.
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationGrupoJavaApplication.class, args);
 	}
