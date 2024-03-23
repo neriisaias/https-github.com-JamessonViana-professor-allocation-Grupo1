82dd575b615c363b77ab980f81c44dfd4d94869e
NairtonMenezes <nairtonmenezesjunior@gmail.com>
2024-03-23 10:44:52 -0300

    Exemplo de test para departamento replicar para outros repositorios.

---
+++ src/test/java/com/projet/professor/allocation/grupoJava/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.projet.professor.allocation.grupoJava.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.projet.professor.allocation.grupoJava.entity.Department;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+public class DepartmentRepositoryTest {
+	@Autowired
+	DepartmentRepository department;
+	
+	@Test
+	void mamute(){
+		List <Department> ltDP = department.findAll();
+		
+		System.out.println(ltDP);
+	}
+}
