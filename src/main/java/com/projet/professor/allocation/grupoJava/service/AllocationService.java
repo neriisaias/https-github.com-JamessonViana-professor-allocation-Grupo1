3080376b446b96056e6ad15077a263c8e1a417a0
NairtonMenezes <nairtonmenezesjunior@gmail.com>
2024-03-23 11:49:09 -0300

    alteração das camadas de serviço

--- src/main/java/com/projet/professor/allocation/grupoJava/service/AllocationService.java
+++ src/main/java/com/projet/professor/allocation/grupoJava/service/AllocationService.java
@@ -1,5 +1,22 @@
 package com.projet.professor.allocation.grupoJava.service;
 
-public class AllocationService {
+import org.springframework.stereotype.Service;
 
+import com.projet.professor.allocation.grupoJava.repository.AllocationRepository;
+
+@Service
+public class AllocationService {
+	private final AllocationRepository allocationRepository;
+	private final ProfessorService professorService;
+	private final CourseService courseService;
+	
+	public AllocationService(AllocationRepository allocationRepository, ProfessorService professorService,
+			CourseService courseService) {
+		super();
+		this.allocationRepository = allocationRepository;
+		this.professorService = professorService;
+		this.courseService = courseService;
+	}
+	
+	
 }
--- src/main/java/com/projet/professor/allocation/grupoJava/service/DepartmentService.java
+++ src/main/java/com/projet/professor/allocation/grupoJava/service/DepartmentService.java
@@ -1,5 +1,8 @@
 package com.projet.professor.allocation.grupoJava.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DepartmentService {
 
 }
